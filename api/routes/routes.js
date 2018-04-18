0ac6dedfaab499ed4d452550b166a5d80de4c8a8
Kogekko <winterspringvnt@gmail.com>
2018-04-18 10:38:00 +0700

    Add files via upload

--- api/routes/routes.js
+++ api/routes/routes.js
@@ -8,7 +8,7 @@ const eventRoute = require('./admin/eventRoute')
 const foodRouteWebsite = require('./website/foodRoute')
 const userRouteWebiste = require('./website/userRoute')
 const loginRouteWebiste = require('./website/loginRoute')
-
+const contactRouteWebsite = require('./website/contactRoute')
 module.exports = app => {
   employeeRoute(app)
   loginRoute(app)
@@ -21,4 +21,5 @@ module.exports = app => {
   foodRouteWebsite(app)
   userRouteWebiste(app)
   loginRouteWebiste(app)
+  contactRouteWebsite(app)
 }
